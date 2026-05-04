7638e81becdce662c8942dc75bd955e0b5644cce
hkshakib <hkshakib.cse@gmail.com>
2026-05-04 12:28:36 +0300

    Lock TempCam to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'src/app/bootstrap.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   await Hive.initFlutter();
   await bootstrap();
 }
